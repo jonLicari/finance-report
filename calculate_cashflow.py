d970c2afa6a1b3e611e99f4c90cc299556e639a0
jonLicari <jlicari@ryerson.ca>
2024-03-02 23:44:42 -0500

    add printing to terminal functions and orchestrators

--- calculate_cashflow.py
+++ calculate_cashflow.py
@@ -5,7 +5,53 @@ from decimal import Decimal
 from expense_class import Cashflow, Expense
 
 
-# def print_to_terminal() -> None:
+def print_cashflow(cashflow_object: Cashflow) -> None:
+    """Print dollar values of Cashflow objects to terminal."""
+    print("   Ingoing:  $", cashflow_object.ingoing)
+    print("   Outgoing: $", cashflow_object.outgoing)
+
+
+def month_index_to_string(index: int) -> str:  # type: ignore
+    """Return the month as a string based on the index."""
+    assert 1 <= index <= 12, "Invalid month index."
+    match index:
+        case 1:
+            return "January"
+        case 2:
+            return "February"
+        case 3:
+            return "March"
+        case 4:
+            return "April"
+        case 5:
+            return "May"
+        case 6:
+            return "June"
+        case 7:
+            return "July"
+        case 8:
+            return "August"
+        case 9:
+            return "September"
+        case 10:
+            return "October"
+        case 11:
+            return "November"
+        case 12:
+            return "December"
+
+
+def print_to_terminal(monthly_totals: list[Cashflow]) -> None:
+    """Orchestrate printing to terminal."""
+    index_of_month: int = 1
+
+    # Print monthly totals
+    for month in monthly_totals:
+        print(month_index_to_string(index_of_month), ":")
+        print_cashflow(month)
+
+    # Print YTD
+    # year_to_date: Cashflow
 
 
 def print_cashflow_totals(expense_list: list[Expense]) -> None:
@@ -95,3 +141,4 @@ def calculations_main(ytd_expenses: list[Expense]) -> None:
     # Monthly Inouts
     monthly_inouts = monthly_inout_sums(ytd_expenses)
     # Print to terminal
+    print_to_terminal(monthly_inouts)
