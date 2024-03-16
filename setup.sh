547b4e5e2b2e1f4ded899d0a81bf012639d4bf4a
jonLicari <jlicari@ryerson.ca>
2024-03-16 13:31:18 -0400

    setup script improvements

--- setup.sh
+++ setup.sh
@@ -3,23 +3,37 @@
 chmod +x setup.sh
 
 # Version
-PYTHON_VERSION="3.11"
+PYTHON_PATH=$(command -v python3 || command -v python)
 
 # Check if Python is installed
-if ! command -v "python$PYTHON_VERSION" &> /dev/null; then
-    echo "Python$PYTHON_VERSION is not installed. Please install Python$PYTHON_VERSION before running this script."
+if [ -z "$PYTHON_PATH" ]; then
+    echo "Python is not installed. Please install Python before running this script."
     exit 1
+else
+    echo "PYTHON_PATH is set to: $PYTHON_PATH"
 fi
 
 # Create a virtual environment
-python$PYTHON_VERSION -m venv venv
+VENV_NAME="finance-script-env"
 
-# Activate the virtual environment
-source venv/bin/activate
+# Are we already in a virtual environment?
+if [ -n "$VIRTUAL_ENV" ]; then
+    deactivate
+
+    # Remove the existing virtual environment directory
+    rm -rf $VIRTUAL_ENV
+fi
+
+# Create & activate new virtual environment
+$PYTHON_PATH -m venv $VENV_NAME --without-pip
+source $VENV_NAME/bin/activate
+
+# install pip
+curl https://bootstrap.pypa.io/get-pip.py | python3
+echo "$VENV_NAME created and active."
 
 # Install dependencies from requirements.txt
-pip3 install -r requirements.txt || pip install -r requirements.txt
+pip3 install -r requirements.txt &>/dev/null
 
 # Display a message indicating successful setup
 echo "Virtual environment created and dependencies installed successfully."
-echo "Activate the virtual environment using: source venv/bin/activate"
